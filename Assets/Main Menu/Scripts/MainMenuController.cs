406c692e7fa7dcafaa1b42ad2badecae4d6d8750
Brynny <111567@student.sae.edu.au>
2016-09-08 11:27:58 +0800

    Started Main Menu Screen

---
+++ Assets/Main Menu/Scripts/MainMenuController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour 
+{
+
+	public void NewGame()
+	{
+
+	}
+
+	public void LoadGame()
+	{
+
+	}
+
+	public void Options()
+	{
+
+	}
+		
+	public void Credits()
+	{
+
+	}
+		
+	public void QuitGame()
+	{
+
+	}
+}
