65f9a5ca0f791136ee470f2daa1ac4ef53eae206
Brynny <111567@student.sae.edu.au>
2016-09-09 13:41:28 +0800

    Desert Level Additions
    Added bulk floating rocks above the beacon, un edited speeds

--- Assets/PlayerScripts/PlayerRaycastShoot.cs
+++ Assets/PlayerScripts/PlayerRaycastShoot.cs
@@ -1,29 +1,29 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class PlayerRaycastShoot : MonoBehaviour 
-{
-
-	public LayerMask whatIsEverything;
-
-	// Use this for initialization
-	void Start () 
-	{
-	
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () 
-	{
-		GunDirection();
-	}
-
-	void GunDirection()
-	{
-		Vector3 gunDirection = transform.TransformDirection(Vector3.forward);
-
-		if (Physics.Raycast(transform.position, gunDirection, out RaycastHit, 100, whatIsEverything)) //100 units forward
-			print("ObjectHit");
-
-	}
-}
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class PlayerRaycastShoot : MonoBehaviour 
+//{
+//
+//	public LayerMask whatIsEverything;
+//
+//	// Use this for initialization
+//	void Start () 
+//	{
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void FixedUpdate () 
+//	{
+//		GunDirection();
+//	}
+//
+//	void GunDirection()
+//	{
+//		Vector3 gunDirection = transform.TransformDirection(Vector3.forward);
+//
+//		if (Physics.Raycast(transform.position, gunDirection, out RaycastHit, 100, whatIsEverything)) //100 units forward
+//			print("ObjectHit");
+//
+//	}
+//}
